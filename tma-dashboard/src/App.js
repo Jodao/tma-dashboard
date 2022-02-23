f366048cdab5cfdf1b743b90a23165ed50128788
Jodao <49023295+Jodao@users.noreply.github.com>
2022-02-23 17:40:37 +0000

    First API communication finalized

--- tma-dashboard/src/App.js
+++ tma-dashboard/src/App.js
@@ -1,7 +1,32 @@
+import { Button } from 'semantic-ui-react'
+import axios from 'axios';
+//import { useEffect } from 'react';
 
 function App() {
+  const baseURL = "http://54.194.189.180:32026/getpartners";
+
+  async function request(){
+    var obj = await axios.get(baseURL)
+    console.log(obj.data)
+  }
+
+ /* useEffect(()=> {
+    request();
+    console.log("segundo");
+  },[]);
+*/
+
+  function handler (ev){
+    ev.preventDefault()
+    request()
+  }
+
   return (
-    <div>Hello!</div>
+    <div>
+      <h1>Hello!</h1>
+
+      <Button onClick={handler}>Primary </Button>
+    </div>
   );
 }
 
