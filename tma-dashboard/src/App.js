1a3e86e02f3af4ccf229601c15860add88fb75fa
Jodao <49023295+Jodao@users.noreply.github.com>
2022-06-14 19:41:23 +0100

    Code Splitting
    Applied code splitting on page routing in order to improve page load performance.

--- tma-dashboard/src/App.js
+++ tma-dashboard/src/App.js
@@ -1,58 +1,64 @@
+import { Suspense, lazy } from 'react';
 import NavBar from "./components/layout/NavBar";
 import {Route, Routes} from 'react-router-dom';
-import ListMetricsPage from './pages/Metrics/ListMetricsPage';
-import ListQualityModelsPage from './pages/QualityModels/ListQualityModelsPage';
-import HomePage from './pages/HomePage'
-import ViewMetricPage from "./pages/Metrics/ViewMetricPage";
-import CreateMetricPage from "./pages/Metrics/CreateMetricPage";
-import ViewQualityModelPage from "./pages/QualityModels/ViewQualityModelPage";
-import CreateConfigurationProfilePage from "./pages/ConfigurationProfiles/CreateConfigurationProfilePage";
-import CreateQualityModelPage from "./pages/QualityModels/CreateQualityModelPage";
-import ViewConfigurationProfilePage from "./pages/ConfigurationProfiles/ViewConfigurationProfilePage";
-import PlotResourceMetricsPage from "./pages/Resources/PlotResourceMetricsPage";
-import SimulateResourceMetricsPage from "./pages/Resources/SimulateResourceMetricsPage";
-import ListRulesPage from "./pages/AdaptationRules/ListRulesPage";
-import CreateRulePage from "./pages/AdaptationRules/CreateRulePage";
-import ViewRulePage from "./pages/AdaptationRules/ViewRulePage";
+
+
+const ListMetricsPage = lazy(() => import('./pages/Metrics/ListMetricsPage'));
+const ListQualityModelsPage = lazy(() => import('./pages/QualityModels/ListQualityModelsPage'));
+const HomePage = lazy(() => import('./pages/HomePage'))
+const ViewMetricPage = lazy(() => import("./pages/Metrics/ViewMetricPage"));
+const CreateMetricPage = lazy(() => import("./pages/Metrics/CreateMetricPage"));
+const ViewQualityModelPage = lazy(() => import("./pages/QualityModels/ViewQualityModelPage"));
+const CreateConfigurationProfilePage = lazy(() => import( "./pages/ConfigurationProfiles/CreateConfigurationProfilePage"));
+const CreateQualityModelPage = lazy(() => import("./pages/QualityModels/CreateQualityModelPage"));
+const ViewConfigurationProfilePage = lazy(() => import("./pages/ConfigurationProfiles/ViewConfigurationProfilePage"));
+const PlotResourceMetricsPage = lazy(() => import("./pages/Resources/PlotResourceMetricsPage"));
+const SimulateResourceMetricsPage = lazy(() => import("./pages/Resources/SimulateResourceMetricsPage"));
+const ListRulesPage = lazy(() => import("./pages/AdaptationRules/ListRulesPage"));
+const CreateRulePage = lazy(() => import("./pages/AdaptationRules/CreateRulePage"));
+const ViewRulePage = lazy(() => import("./pages/AdaptationRules/ViewRulePage"));
 
 function App() {
+
   return (
     <div>
-      <NavBar/>     
-      <Routes>
-        <Route path="/" exact element={<HomePage/>}></Route>
+      <NavBar/>
+      <Suspense fallback={<div>Loading...</div>}>     
+        <Routes>
+          <Route path="/" exact element={<HomePage/>}></Route>
 
 
-        
-        <Route path="/createMetric" element={<CreateMetricPage/>}></Route>
-        
-        <Route path="/getMetrics/:id" element={<ViewMetricPage/>}></Route>
+          
+          <Route path="/createMetric" element={<CreateMetricPage/>}></Route>
+          
+          <Route path="/getMetrics/:id" element={<ViewMetricPage/>}></Route>
 
-        <Route path="/getMetrics" element={<ListMetricsPage currpath="/getMetrics"/>}></Route>
-
-
-
-        <Route path="/createQualityModel" element={<CreateQualityModelPage/>}></Route>
-        
-        <Route path="/getQualityModels/:id" element={<ViewQualityModelPage/>}></Route>
-        
-        <Route path="/getQualityModels" element={<ListQualityModelsPage currpath="/getQualityModels"/>}></Route>
-
-        
-        
-        <Route path="/createConfigurationProfile" element={<CreateConfigurationProfilePage/>}></Route>
-        
-        <Route path="/getConfigurationProfile/:id" element={<ViewConfigurationProfilePage/>}></Route>
+          <Route path="/getMetrics" element={<ListMetricsPage currpath="/getMetrics"/>}></Route>
 
 
-        <Route path="/getResources" element={<PlotResourceMetricsPage />}></Route>
-        <Route path="/simulateMetrics" element={<SimulateResourceMetricsPage />}></Route>
 
-        <Route path="/getAdaptationRules" element={<ListRulesPage currpath="/getAdaptationRules"/>}></Route>
-        <Route path="/getAdaptationRules/:ruleName" element={<ViewRulePage/>}></Route>
-        <Route path="/createAdaptationRule" element={<CreateRulePage/>}></Route>
+          <Route path="/createQualityModel" element={<CreateQualityModelPage/>}></Route>
+          
+          <Route path="/getQualityModels/:id" element={<ViewQualityModelPage/>}></Route>
+          
+          <Route path="/getQualityModels" element={<ListQualityModelsPage currpath="/getQualityModels"/>}></Route>
 
-      </Routes>
+          
+          
+          <Route path="/createConfigurationProfile" element={<CreateConfigurationProfilePage/>}></Route>
+          
+          <Route path="/getConfigurationProfile/:id" element={<ViewConfigurationProfilePage/>}></Route>
+
+
+          <Route path="/getResources" element={<PlotResourceMetricsPage />}></Route>
+          <Route path="/simulateMetrics" element={<SimulateResourceMetricsPage />}></Route>
+
+          <Route path="/getAdaptationRules" element={<ListRulesPage currpath="/getAdaptationRules"/>}></Route>
+          <Route path="/getAdaptationRules/:ruleName" element={<ViewRulePage/>}></Route>
+          <Route path="/createAdaptationRule" element={<CreateRulePage/>}></Route>
+
+        </Routes>
+      </Suspense>
     </div>
   );
 }
